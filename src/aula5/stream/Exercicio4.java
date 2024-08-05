da4eb9bb180e48da161811a0f675d9a6021cea8e
Tarcísio Luiz Pereira Bogo <131721143+tk4500@users.noreply.github.com>
2024-08-05 17:29:53 -0300

    Update Exercicio4.java

--- src/aula5/stream/Exercicio4.java
+++ src/aula5/stream/Exercicio4.java
@@ -2,6 +2,7 @@ package aula5.stream;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,10 @@ public class Exercicio4 {
 		List<Pessoa> pessoas = new ArrayList<>();
 		String[] nome = {"Caio","Miguel","Priscilla","Tiffany","Pablo","Manito","Pedro","Adalberto","Betanio","Joliscreusa"};
 		String[] telefone = {"99934-7855","99754-7855","93534-7555","99963-7855","96934-7955","99925-7125","99934-2355","94934-7755","99534-7855","91934-7255"};
-		LocalDate[] dataNascimento = {LocalDate.parse("2006-04-30"),LocalDate.parse("2007-03-15"),LocalDate.parse("2005-04-21"),LocalDate.parse("2006-06-11"),LocalDate.parse("2003-03-15"),LocalDate.parse("2007-04-30"),LocalDate.parse("1689-02-21"),LocalDate.parse("1687-08-10"),LocalDate.parse("1975-12-19"),LocalDate.parse("2015-10-01"),};
+		LocalDate[] dataNascimento = {LocalDate.parse("2006-05-30"),LocalDate.parse("2007-03-15"),LocalDate.parse("2005-04-21"),LocalDate.parse("2006-06-11"),LocalDate.parse("2003-03-15"),LocalDate.parse("2007-04-30"),LocalDate.parse("1689-02-21"),LocalDate.parse("1687-08-10"),LocalDate.parse("1975-12-19"),LocalDate.parse("2015-10-01"),};
 		for(int i=0;i<10;i++) 
 			pessoas.add(new Pessoa(nome[i],telefone[i],dataNascimento[i]));
-		System.out.println(pessoas.stream().filter(p->p.dataNascimento.getMonthValue() == 5).map(Pessoa::getNome).sorted().collect(Collectors.joining(", ")));
+		pessoas.stream().filter(p->p.dataNascimento.getMonthValue() == 5).sorted(Comparator.comparing(Pessoa::getNome)).forEach(n->System.out.println("Nome: " + n.nome + "\nTelefone: " + n.telefone + "\nData de Nascimento: " + n.dataNascimento.toString()));;
 		
 	}
 
