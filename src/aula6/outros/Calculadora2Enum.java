9687a5cf5cfe4d93792a09e6f54985ede8b577f6
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-08-07 10:11:44 -0300

    aula_6_enum

--- src/aula6/outros/Calculadora2Enum.java
+++ src/aula6/outros/Calculadora2Enum.java
@@ -3,7 +3,9 @@ package aula6.outros;
 import java.util.function.BiFunction;
 
 public enum Calculadora2Enum {
-	SOMA("soma", (a, b) -> a + b), SUBTRACAO("subtração", (a, b) -> a - b), DIVISAO("divisão", (a, b) -> a / b),
+	SOMA("soma", (a, b) -> a + b),
+	SUBTRACAO("subtração", (a, b) -> a - b),
+	DIVISAO("divisão", (a, b) -> a / b),
 	MULTIPLICACAO("multiplicação", (a, b) -> a * b);
 
 	private String nomeAmigavel;
